74299e7638da316c82f755ec8af0cc9aa00beadb
sanjaymahanta <sanjaymahantait@gmail.com>
2024-12-07 16:44:49 +0530

    fifth commit

--- src/test/java/com/MouseActions/PageTransition.java
+++ src/test/java/com/MouseActions/PageTransition.java
@@ -13,22 +13,29 @@ import org.testng.annotations.Test;
 
 public class PageTransition {
 
-	
 	WebDriver driver;
-	
+
 	@BeforeClass
 	public void setup() {
 		System.setProperty("webdriver.gecko.driver", "D:/geckodriver133.exe");
 		driver = new FirefoxDriver();
 		driver.get("https://www.tatacliq.com/");
-		
-		}
-	
+
+	}
+
 	@Test
 	public void flow() throws InterruptedException {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-driver.findElement(By.xpath("(//div[text()='Gift Card'])[2]")).click();
-		
+		try {
+			// Wait for the second "Gift Card" element to be visible
+			WebElement giftCardElement = wait
+					.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[text()='Gift Card'])[2]")));
+			giftCardElement.click();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			driver.quit();
+		}
 	}
-	
-	}
+}
